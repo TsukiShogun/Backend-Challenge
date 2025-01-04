30105bdc7100f837bd9a6dcd7859ec3e07a2fbf3
TsukiShogun <gabrielxilim@gmail.com>
2025-01-04 08:50:57 -0300

    initial commit

---
+++ microservicepokeapiV2/src/main/java/looqbox/challenge/microservicepokeapi/MicroservicepokeapiApplication.java
@@ -0,0 +1,13 @@
+package looqbox.challenge.microservicepokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicepokeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicepokeapiApplication.class, args);
+	}
+
+}
---
+++ microservicepokeapiV2/src/test/java/looqbox/challenge/microservicepokeapi/MicroservicepokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package looqbox.challenge.microservicepokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicepokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
